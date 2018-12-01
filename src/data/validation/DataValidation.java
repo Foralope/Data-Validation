ad33baa1281e4982631db1eeafa3ba0cc20c20e9
Foralope <43345303+Foralope@users.noreply.github.com>
2018-12-01 12:39:44 -0500

    Update DataValidation.java

--- src/data/validation/DataValidation.java
+++ src/data/validation/DataValidation.java
@@ -18,6 +18,7 @@ public class DataValidation {
      * @param input The string that takes in input.
      */
     public static void containsAn_a (String needsAn_a, String input) {
+        //Need an 'a' to continue
         input = JOptionPane.showInputDialog("Enter a string that contains a lowercase 'a'");
         needsAn_a = input;
         while (Pattern.compile("[a]").matcher(needsAn_a).find() == false){
@@ -35,13 +36,13 @@ public class DataValidation {
         //Doesn't let you leave until you enter valid data (no numbers and longer than six characters)
         input = JOptionPane.showInputDialog("Using letters only, enter a word that is longer than 6 letters without numbers.");
         greaterThanSix = input;
-        while (greaterThanSix.length() <= 6){
-            System.err.println("Your word is not long enough.");
-            input = JOptionPane.showInputDialog("Using letters only, enter a word that is longer than 6 letters without numbers.");
-            greaterThanSix = input;
-        }
-        while (Pattern.compile("[0-9]").matcher(greaterThanSix).find() == true){
-            System.err.println("Your word contains numbers. Please input a word without numbers.");
+        while (greaterThanSix.length() <= 6 || Pattern.compile("[0-9]").matcher(greaterThanSix).find() == true){
+            if (greaterThanSix.length() <= 6) {
+                System.err.println("Your word is not long enough.");
+            }
+            if (Pattern.compile("[0-9]").matcher(greaterThanSix).find() == true) {
+                System.err.println("Your word contains numbers. Please input a word without numbers.");
+            }
             input = JOptionPane.showInputDialog("Using letters only, enter a word that is longer than 6 letters without numbers.");
             greaterThanSix = input;
         }
@@ -61,29 +62,18 @@ public class DataValidation {
         
         input = JOptionPane.showInputDialog("Enter a string between 5 and 15 characters long that doesn't contain a 'Z'.");
         betweenFiveAndFifteen = input;
-        while (0 == 0){
-            while (betweenFiveAndFifteen.length() < 5){
+        while (betweenFiveAndFifteen.length() < 5 || betweenFiveAndFifteen.length() > 15 || Pattern.compile("[Z]").matcher(greaterThanSix).find() == true){
+            if (betweenFiveAndFifteen.length() < 5){
                 System.err.println("Your string is too short.");
-                input = JOptionPane.showInputDialog("Enter a string between 5 and 15 characters long that doesn't contain a 'Z'.");
-                betweenFiveAndFifteen = input;
             }
-            while (betweenFiveAndFifteen.length() > 15){
+            if (betweenFiveAndFifteen.length() > 15){
                 System.err.println("Your string is too long.");
-                input = JOptionPane.showInputDialog("Enter a string between 5 and 15 characters long that doesn't contain a 'Z'.");
-                betweenFiveAndFifteen = input;
             }
-            while (Pattern.compile("[Z]").matcher(greaterThanSix).find() == true){
+            if (Pattern.compile("[Z]").matcher(greaterThanSix).find() == true){
                 System.err.println("Your string contains the letter 'Z'. Please remove it.");
-                input = JOptionPane.showInputDialog("Enter a string between 5 and 15 characters long that doesn't contain a 'Z'.");
-                betweenFiveAndFifteen = input;
             }
-            if (Pattern.compile("[Z]").matcher(greaterThanSix).find() == false){
-                if (betweenFiveAndFifteen.length() < 15){
-                    if (betweenFiveAndFifteen.length() > 5){
-                    }
-                }
-            }
-            //Need way to break out//
+            input = JOptionPane.showInputDialog("Enter a string between 5 and 15 characters long that doesn't contain a 'Z'.");
+            betweenFiveAndFifteen = input;
         }
     }
     
